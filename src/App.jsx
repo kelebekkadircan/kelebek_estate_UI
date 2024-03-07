2d3489367fb448691991e06dd1a0262335fc71e3
kelebekkadircan <kelebekkadircan@gmail.com>
2024-03-07 20:48:38 +0300

    initialize and responsiveness configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components";
+import "./layout.scss";
+
+function App() {
+  return (
+    <>
+      <div className="layout">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">Hello left</div>
+      <div className="right">Hello right</div>
+    </nav>
+  );
+};
